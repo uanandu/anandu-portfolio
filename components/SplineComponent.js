f06315dc89a1aff1e7622e1230d1b67bd9120c29
Ananda Narayanan Udayakumar <ananduun@icloud.com>
2022-08-23 16:29:16 -0400

    styling

--- components/SplineComponent.js
+++ components/SplineComponent.js
@@ -3,9 +3,12 @@ import { Stack, VStack } from "@chakra-ui/react";
 
 const SplineComponent = () => {
   return (
-    <Stack h="85vh" w="100%"
+    <Stack h="80vh" w="70%"
+    marginTop="5vh"
+    border="1px solid black"
     >
-      <Spline scene="https://prod.spline.design/ys5vPcV6gtFKk0cN/scene.splinecode" />
+      {/* <Spline scene="https://prod.spline.design/ys5vPcV6gtFKk0cN/scene.splinecode" /> */}
+      <iframe  loading="lazy" src='https://my.spline.design/untitled-7468cee9eec15f5d54a3ff3ed2793adc/' width='100%' height='100%'></iframe>
     </Stack>
   );
 };
