a98d36b29a44d636af768d281c84b847b8d38456
Ananda Narayanan Udayakumar <ananduun@icloud.com>
2022-08-25 13:02:28 -0400

    styling

--- components/CurrentProjects.js
+++ components/CurrentProjects.js
@@ -1,10 +1,24 @@
-import { VStack } from "@chakra-ui/react"
+import { Image, ListItem, Stack, Text, UnorderedList, VStack } from "@chakra-ui/react"
 
 const CurrentProjects = () => {
     return (
         <>
             <VStack>
-                <h1>Current Projects</h1>
+                <Text fontSize="3xl" fontWeight="bold">Current Projects</Text>
+                <Stack>
+                    <Text fontSize="xl" fontWeight="bold">Construction Code 2.0</Text>
+                    <Text fontSize="xl" fontWeight="bold">Client: Michael Belanger, Contractor</Text>
+                    <Text fontSize="xl" fontWeight="bold">Field: Construction</Text>
+                    <Text fontSize="lg" fontWeight="bold">Status: In Progress</Text>
+                    <Image src="https://cdn.discordapp.com/attachments/979724631172849685/1012400782907277322/Screen_Shot_2022-08-25_at_12.39.12_PM.png" />
+                    <Text fontSize="lg" fontWeight="bold">Description: Construction company website</Text>
+                    <UnorderedList>
+                        <Text fontSize="lg" fontWeight="bold">It features the following:</Text>
+                        <ListItem>Company landing page</ListItem>
+                        <ListItem>Company contact page</ListItem>
+                        <ListItem>Previous works</ListItem>
+                    </UnorderedList>
+                </Stack>
             </VStack>
         </>
     )
